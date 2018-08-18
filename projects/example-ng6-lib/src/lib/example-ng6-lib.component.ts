ff89dd56e17d47988e5fed5dfea8c9d23951d13f
ogm710811 <orestes.garcia.martinez@gmail.com>
2018-08-17 23:31:11 -0400

    adding templateUrl and styleUrls to example-ng6-lib.component

--- projects/example-ng6-lib/src/lib/example-ng6-lib.component.ts
+++ projects/example-ng6-lib/src/lib/example-ng6-lib.component.ts
@@ -2,18 +2,11 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'enl-example-ng6-lib',
-  template: `
-    <p>
-      example-ng6-lib works!
-    </p>
-  `,
-  styles: []
+  templateUrl: './example-ng6-lib.component.html',
+  styleUrls: ['./example-ng6-lib.component.css']
 })
 export class ExampleNg6LibComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ExampleNg6LibModule } from 'example-ng6-lib';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, ExampleNg6LibModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
